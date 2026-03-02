554b859889badf920d70cd0dd70104147de2eb6c
MarianaSae <ysaenz0510@gmail.com>
2026-03-01 23:01:49 -0700

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['rol'])){
+ header("Location: login.php");
+ exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Sistema Escolar</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<?php include("navbar.php"); ?>
+
+<div class="container mt-4">
---
+++ includes/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg custom-navbar px-4 shadow">
+
+<a class="navbar-brand" href="panel_admin.php">
+🎓 Control Escolar
+</a>
+
+<div class="ms-auto">
+<a href="panel_admin.php" class="btn btn-light me-2">
+🏠 Inicio
+</a>
+
+<a href="logout.php" class="btn btn-danger">
+Cerrar sesión
+</a>
+</div>
+
+</nav>
